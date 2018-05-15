5dbcf25a49e48cbcdb2a582ae711d53fbc8375a5
Sam Jones <sam.jones494@googlemail.com>
2018-05-15 15:38:15 +0100

    raise and lower temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,4 +8,26 @@ describe('Thermostat', function() {
   it('starts with a temperature of 20', function() {
     expect(thermostat.currentTemp()).toEqual(20);
   });
+
+  describe('#up', function() {
+    it('raises the current temperature by 1', function() {
+      thermostat.up();
+      expect(thermostat.currentTemp()).toEqual(21);
+    });
+  });
+
+  describe('#down', function() {
+    it('lowers the current temperature by 1', function() {
+      thermostat.down();
+      expect(thermostat.currentTemp()).toEqual(19);
+    });
+
+    it('will not lower the temperature below the minimum temperature', function() {
+      var distanceFromMin = thermostat.currentTemp() - thermostat._minTemp;
+      for(var i = 0; i <= distanceFromMin; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.currentTemp()).toEqual(thermostat._minTemp);
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,18 @@
 function Thermostat(){
   this._currentTemp = 20;
+  this._minTemp = 10;
 }
 
 Thermostat.prototype.currentTemp = function() {
   return this._currentTemp;
+}
+
+Thermostat.prototype.up = function() {
+  this._currentTemp++;
+}
+
+Thermostat.prototype.down = function() {
+  if (this.currentTemp() > this._minTemp) {
+    this._currentTemp--;
+  }
 }
