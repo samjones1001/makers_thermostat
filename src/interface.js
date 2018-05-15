521a1ede94732075af518e33968c0daac0b0e210
Sam Jones <sam.jones494@googlemail.com>
2018-05-15 18:48:50 +0100

    basic api call

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   let thermostat = new Thermostat()
   setTempDisplay();
   setPsDisplay();
+  setLocalWeather();
 
   $('#temp-up').on('click', function() {
     thermostat.up();
@@ -32,5 +33,12 @@ $(document).ready(function() {
   function setPsDisplay() {
     let content = thermostat.isPowerSaving() ? 'on' : 'off'
     $('#ps-display').text(content);
+  }
+
+  function setLocalWeather() {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=28da720ac77959beb32c9a1a92ebf7d7', function(res) {
+      $('#weather-temp').text(res.main.temp)
+      $('#weather-description').text(res.weather[0].main.toLowerCase());
+    })
   }
 });
